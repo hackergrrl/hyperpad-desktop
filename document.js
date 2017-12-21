85a30d757a088677389bcaa5aae69e2daf56e6c9
Stephen Whitmore <sww@eight45.net>
2017-12-21 10:26:53 -0800

    load up a basic in-memory hyper-string

--- document.js
+++ document.js
@@ -1,4 +1,6 @@
 var Quill = require('quill')
+var hstring = require('hyper-string')
+var memdb = require('memdb')  // TODO(noffle): use real level storage
 
 module.exports = function () {
   var editor = new Quill('#editor', {
@@ -10,5 +12,13 @@ module.exports = function () {
   editorElement.style['font-family'] = '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace'
 
   editor.focus()
+
+  var str = hstring(memdb())
+  str.insert(null, null, 'Hello world', function () {
+    str.text(function (err, text) {
+      if (err) throw err
+      editor.insertText(0, text)
+    })
+  })
 }
 
