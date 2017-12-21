6d9d426d6a75c8fc267c9e0885d5e8f302e278ba
Stephen Whitmore <sww@eight45.net>
2017-12-21 10:53:55 -0800

    dev: apply local edits to hyper-string

--- document.js
+++ document.js
@@ -24,11 +24,46 @@ module.exports = function () {
   var docPath = path.join(userDataPath, docId)
 
   var str = hstring(level(docPath))
-  str.insert(null, null, 'Hello world', function () {
     str.text(function (err, text) {
       if (err) throw err
       editor.insertText(0, text)
+      listenForEdits()
     })
-  })
+
+  function listenForEdits () {
+    // TODO: lock editor until changes are processed
+    editor.on('text-change', function (delta, oldDelta, source) {
+      var pos = 0
+      var opIdx = 0
+      ;(function next (err) {
+        if (err) throw err
+        var op = delta.ops[opIdx]
+        if (!op) return
+        opIdx++
+
+        str.chars(function (err, chars) {
+          if (err) throw err
+          // console.log('chars', chars)
+
+          if (op.retain) {
+            pos += op.retain
+            next()
+          }
+          if (op.insert) {
+            var after = pos > 0 ? chars[pos - 1].pos : null
+            var before = pos < chars.length ? chars[pos].pos : null
+            // console.log('insert', after, before, op.insert)
+            str.insert(after, before, op.insert, next)
+          }
+        })
+      })()
+
+      if (source == 'api') {
+        console.log("An API call triggered this change.");
+      } else if (source == 'user') {
+        console.log("A user action triggered this change.");
+      }
+    })
+  }
 }
 
