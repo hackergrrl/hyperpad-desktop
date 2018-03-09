f87cd8c89a3548a5ea12e17f600f25c04c32dba3
Stephen Whitmore <sww@eight45.net>
2018-03-09 14:51:33 -0800

    choo-ify!

--- document-list.js
+++ document-list.js
@@ -1,70 +1,41 @@
-var bel = require('bel')
-var morph = require('morphdom')
+var html = require('choo/html')
 
-module.exports = function (onSelected) {
-
-  var docs = []
-  var selectedIdx = -1
-  var element = renderList(docs, selectedIdx)
-
-  element.updateDocs = function (items) {
-    docs = items
-    update()
-  }
-
-  element.updateSelection = function (idx) {
-    selectDoc(docs[idx], idx)
-  }
-
-  return element
-
-  function update () {
-    morph(element, renderList(docs, selectedIdx))
-  }
-
-  function renderList (elms, selected) {
-    var extra = [renderCreateDocRow(), renderAddDocRow()]
-    return bel`<div class="doclist">
-      ${extra.concat(elms.map(function (elm, i) {
-        return renderListDoc(elm, i, selected === i)
+module.exports = function (state, emit) {
+  var extra = [renderCreateDocRow(), renderAddDocRow()]
+  return html`
+    <div class="doclist">
+      ${extra.concat(state.documents.map(function (elm, i) {
+        return renderListDoc(elm, i, state.selectedDocumentIdx === i)
       }))}
-      </div>`
-  }
+    </div>
+  `
 
   function renderCreateDocRow () {
-    return bel`<div class="docitem-special" onclick=${onClickCreate}>
+    return html`<div class="docitem-special" onclick=${onClickCreate}>
       <div class="docitem-contents">Create new pad</div>
       </div>`
+    function onClickCreate () {
+      emit('createDocument')
+    }
   }
 
   function renderAddDocRow () {
-    return bel`<div class="docitem-special" onclick=${onClickAdd}>
+    return html`<div class="docitem-special" onclick=${onClickAdd}>
       <div class="docitem-contents">Add pad from hash</div>
       </div>`
+    function onClickAdd () {
+      emit('addDocument')
+    }
   }
 
   function renderListDoc (elm, i, selected) {
     var clazz = 'docitem'
     if (selected) clazz = 'docitem-selected'
-    return bel`<div class="${clazz}" onclick=${onClick}>
+    return html`<div class="${clazz}" onclick=${onClick}>
       <div class="docitem-contents">${elm}</div>
     </div>`
     function onClick () {
-      selectDoc(docs[i], i)
+      emit('selectDocument', i)
     }
-  }
-
-  function onClickCreate () {
-    onSelected('&create')
-  }
-
-  function onClickAdd () {
-    onSelected('&add')
-  }
-
-  function selectDoc (elm, i) {
-    selectedIdx = i
-    update()
-    onSelected(elm)
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Quill = require('quill')
 var Doc = require('./document')
-var createDocumentList = require('./document-list')
+var renderDocumentList = require('./document-list')
 var getLocalDocs = require('./get-local-docs')
 var randomBytes = require('randombytes')
 var hstring = require('hyper-string')
@@ -8,69 +8,106 @@ var level = require('level')
 var path = require('path')
 var ipc = require('electron').ipcRenderer
 
-// TODO: turn this source file into a choo-like/bel-like component
+var choo = require('choo')
+var html = require('choo/html')
 
 var editor
 var currentDoc
-var docSidebar = createDocumentList(onSelected)
-document.getElementById('left').appendChild(docSidebar)
 
-refreshDocList(start)
+var app = choo()
+app.use(function (state, emitter) {
+  state.documents = []
+  state.selectedDocumentIdx = -1
 
-function refreshDocList (cb) {
+  setTimeout(function () {
+    state.editor = new Quill('#editor', {
+      modules: { toolbar: '' },
+      theme: 'snow'
+    })
+  }, 500)
+
   getLocalDocs(function (err, docs) {
     if (err) throw err
-    docSidebar.updateDocs(docs)
-    cb()
+    state.documents = docs
+    emitter.emit('render')
+  })
+
+  emitter.on('createDocument', function () {
+    console.log('event: createDocument')
+    createDocument()
+  })
+
+  emitter.on('addDocument', function (hash) {
+    console.log('event: addDocument')
+    addDocument(hash)
+  })
+
+  emitter.on('selectDocument', function (i) {
+    console.log('event: selectDocument', i)
+    state.selectedDocumentIdx = i
+    emitter.emit('render')
+    selectDocument(state.documents[i], state.editor)
+  })
+})
+app.route('/', mainView)
+app.mount('body')
+
+function mainView (state, emit) {
+  return html`
+    <body>
+      <div id="left" class="left-side">
+        ${renderDocumentList(state, emit)}
+      </div>
+      <div id="right" class="right-side">
+        <h1 id="doc-title">Untitled hyperpad document</h1>
+        ${renderEditor()}
+      </div>
+    </body>
+  `
+}
+
+function renderEditor () {
+  var editorElement = document.createElement("div")
+  editorElement.id = 'editor'
+  editorElement.style.height = (window.innerHeight - 100) + 'px'
+  editorElement.style.width = (window.innerWidth - 0) + 'px'
+  editorElement.classList.add('editor')
+  editorElement.style['font-family'] = '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace'
+  return editorElement
+}
+
+function createDocument () {
+  var name = randomBytes(20).toString('hex')
+  var userDataPath = ipc.sendSync('get-user-data-path')
+  var docPath = path.join(userDataPath, name)
+  var db = level(docPath)
+  var str = hstring(db)
+  // TODO: break this out into hyper-doc module (named document)
+  str.log.append({'doc-id': name, 'doc-name': name}, function (err) {
+    db.close(function () {
+      getLocalDocs(function (err, docs) {
+        if (err) throw err
+        docSidebar.updateDocs(docs)
+        docSidebar.updateSelection(docs.indexOf(name))
+      })
+    })
   })
 }
 
-function start () {
-  var editorElement = document.createElement("div");
-  editorElement.id = 'editor'
-  document.getElementById('right').appendChild(editorElement);
-  editor = new Quill('#editor', {
-    modules: { toolbar: '' },
-    theme: 'snow'
-  });
-  editorElement.style.height = (window.innerHeight - 100) + 'px';
-  editorElement.style.width = (window.innerWidth - 0) + 'px';
-  editorElement.classList.add('editor')
-  editorElement.style['font-family'] = '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace'
+function addDocument () {
+  alert('TODO: implement me')
 }
 
-function onSelected (hash) {
-  console.log('SELECTED', hash)
-
-  if (hash === '&create') {
-    var hash = randomBytes(20).toString('hex')
-    var userDataPath = ipc.sendSync('get-user-data-path')
-    var docPath = path.join(userDataPath, hash)
-    var db = level(docPath)
-    var str = hstring(db)
-    // TODO: break this out into hyper-doc module (named document)
-    str.log.append({'doc-id': hash, 'doc-name': hash}, function (err) {
-      db.close(function () {
-        getLocalDocs(function (err, docs) {
-          if (err) throw err
-          docSidebar.updateDocs(docs)
-          docSidebar.updateSelection(docs.indexOf(hash))
-        })
-      })
-    })
-  } else if (hash === '&add') {
-    alert('TODO: implement me')
-  } else {
-    document.getElementById('doc-title').innerText = hash
-    if (currentDoc) {
-      currentDoc.unregister(function () {
-        var userDataPath = ipc.sendSync('get-user-data-path')
-        currentDoc = Doc(path.join(userDataPath, hash), editor)
-      })
-    } else {
+function selectDocument (name, editor) {
+  document.getElementById('doc-title').innerText = name
+  if (currentDoc) {
+    currentDoc.unregister(function () {
       var userDataPath = ipc.sendSync('get-user-data-path')
-      currentDoc = Doc(path.join(userDataPath, hash), editor)
-    }
+      currentDoc = Doc(path.join(userDataPath, name), editor)
+    })
+  } else {
+    var userDataPath = ipc.sendSync('get-user-data-path')
+    currentDoc = Doc(path.join(userDataPath, name), editor)
   }
 }
 
